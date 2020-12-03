48f1cacf9444956c8db0b032702bece1513a2ab2
breanna-rayburn <breanna.rayburn00@gmail.com>
2020-12-03 11:54:17 -0800

    hashrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter, Route, BrowserRouter as Router } from 'react-router-dom';
 import Home from './components/Home/Home';
 import Services from './components/Services/Services';
 import Equipment from './components/Equipment/Equipment';
@@ -8,14 +8,16 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-      <Router>
-        <Route exact path="/" component={Home}/>
-        <Route path="/services" component={Services}/>
-        <Route path="/equipment" component={Equipment}/>
-        <Route path="/contact" component={Contact}/>
-      </Router>
-    </div>
+    <HashRouter basename="/">
+      <div>
+        <Router>
+          <Route exact path="/" component={Home}/>
+          <Route path="/services" component={Services}/>
+          <Route path="/equipment" component={Equipment}/>
+          <Route path="/contact" component={Contact}/>
+        </Router>
+      </div>
+    </HashRouter>
   );
 }
 
