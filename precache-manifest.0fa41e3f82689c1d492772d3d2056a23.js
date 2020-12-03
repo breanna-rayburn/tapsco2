a7bd7987dd8814ae9e71647649e300fc0d0efecd
breanna-rayburn <breanna.rayburn00@gmail.com>
2020-12-03 11:47:53 -0800

    Updates

---
+++ precache-manifest.0fa41e3f82689c1d492772d3d2056a23.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5c0e77c7130849a9e5b035cfff5d5fc",
+    "url": "/tapsco2/index.html"
+  },
+  {
+    "revision": "6d0ba2165f9821a1af01",
+    "url": "/tapsco2/static/css/main.2d530ea0.chunk.css"
+  },
+  {
+    "revision": "cac58525e08bdf60ccf6",
+    "url": "/tapsco2/static/js/2.9beb91b3.chunk.js"
+  },
+  {
+    "revision": "0dc713a05373e5c9a88cb999f31ae6e4",
+    "url": "/tapsco2/static/js/2.9beb91b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d0ba2165f9821a1af01",
+    "url": "/tapsco2/static/js/main.4ee12fee.chunk.js"
+  },
+  {
+    "revision": "cfe73245a1224907d4f2",
+    "url": "/tapsco2/static/js/runtime-main.5de52a0f.js"
+  },
+  {
+    "revision": "fc0c2bb286bfee063612f59f8c60626a",
+    "url": "/tapsco2/static/media/taps-main-header.fc0c2bb2.jpg"
+  },
+  {
+    "revision": "75de675ad95f7ef3d6a71584ea4bc94c",
+    "url": "/tapsco2/static/media/tapsco2-logo-w-glow.75de675a.png"
+  }
+]);
